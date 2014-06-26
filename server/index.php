8e6e72fb941e0364a31392fbe9fa8ddf9703efab
Patrick Valle <git@patrickvalle.com>
2014-06-26 08:13:12 -0400

    Moving new db credentials out of source control

--- server/index.php
+++ server/index.php
@@ -1,27 +1,25 @@
 <?php
 
 // Import(s)
+require 'config.php';
 require 'vendor/autoload.php';
 require 'scripts/services/PhotosService.php';
 require 'scripts/controllers/PhotosController.php';
-
-// Constant(s)
-$PHOTOS_PATH = './assets/images/photos';
 
 // Register a new Slim app
 $app = new \Slim\Slim();
 
 // Medoo database settings
 $database = new medoo(array(
-  'database_type' => 'mysql',
-  'database_name' => 'evelynvalle',
-  'server' => 'database.evelynvalle.com',
-  'username' => 'user',
-  'password' => 'pass'
+  'database_type' => $CONFIG['database']['type'],
+  'database_name' => $CONFIG['database']['name'],
+  'server'        => $CONFIG['database']['host'],
+  'username'      => $CONFIG['database']['user'],
+  'password'      => $CONFIG['database']['pass']
 ));
 
 // Initialize service(s)
-$photosService = new PhotosService($database, $PHOTOS_PATH);
+$photosService = new PhotosService($database, $CONFIG['app']['photos_path']);
 
 // Initialize controller(s)
 $photosController = new PhotosController($app, $photosService);
